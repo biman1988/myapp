9f7b7384f7d7ad49fae83e251aa6409cc5787795
Shibaji Debnath <imshibaji@gmail.com>
2018-04-21 18:11:47 +0530

    update

--- routes/admin.js
+++ routes/admin.js
@@ -22,6 +22,16 @@ router.get('/user/add', function (req, res, next) {
 });
 
 router.post('/user/add', function(req, res, next){
+    if (!req.body.username){
+        res.render('admin/users/add', { title: 'Users Add Page', message: "Username Required" });
+    }else if(!req.body.password){
+        res.render('admin/users/add', { title: 'Users Add Page', message: "Password Required"});
+    } else if (req.body.password.length < 6){
+        res.render('admin/users/add', { title: 'Users Add Page', message: "Minimum 6 charector is Required" });
+    }else{
+        next();
+    }
+}, function(req, res, next){
     var u = new User(req.body);
     u.save().then(function(data){
         console.log(data);
@@ -29,6 +39,7 @@ router.post('/user/add', function(req, res, next){
         res.redirect('/admin/users');
     }).catch(function(err){
         console.log(err);
+        res.render('admin/users/add', { title: 'Users Add Page', message: "Database Error: "+err.message });
     });
 });
 
@@ -42,6 +53,13 @@ router.get('/user/edit/:id', function(req, res, next){
 
 router.post('/user/edit', function(req, res, next){
     User.findByIdAndUpdate(req.body.id, req.body).then(data => {
+        console.log(data);
+        res.redirect('/admin/users');
+    });
+});
+
+router.get('/user/delete/:id', function (req, res, next) {
+    User.findByIdAndRemove(req.params.id, function (err, data) {
         console.log(data);
         res.redirect('/admin/users');
     });
